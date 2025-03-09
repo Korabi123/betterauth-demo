360df9306ce5013a7250f20955cc29307d5bf179
Korab Imeri <korabimeri91@gmail.com>
2025-03-09 13:40:46 +0100

    fix: fixed a build error where the useSearchParams hook needed to be wrapped in a suspense boundary

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
 import { RegisterCard } from "@/components/auth/register-card";
+import { Suspense } from "react";
 
 const SignUpPage = () => {
   return (
     <div className="h-[100vh] z-50 w-full flex flex-col items-center justify-center">
-      <RegisterCard />
+      <Suspense>
+        <RegisterCard />
+      </Suspense>
     </div>
   );
 }
